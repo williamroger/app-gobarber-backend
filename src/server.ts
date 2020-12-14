5cda8b89dac3d11a306921ae955978b9da21063e
William Roger <williamroger.frontend@gmail.com>
2020-12-14 00:17:16 -0300

    Cadastrando agendamentos e Validando Datas

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
+/* eslint-disable no-console */
 import express from 'express';
 import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello GoStack!'});
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port:  3333 ⚡️');
+  console.log('🚀 Server started on port:  3333 🔥');
 });
