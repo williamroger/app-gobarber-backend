035a368de095a24341d09d2d4ed11a508573414e
William Roger <williamroger.frontend@gmail.com>
2021-01-10 00:17:32 -0300

    Atualizando avatar no banco de dados

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,6 +4,7 @@ import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const userRouter = Router();
 const upload = multer(uploadConfig);
@@ -33,8 +34,18 @@ userRouter.patch('/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
-  return response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
 });
 
 export default userRouter;
