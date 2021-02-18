a6657af547cf4d7cfecc31ee16590aa9e31f5398
William Roger <williamroger.frontend@gmail.com>
2021-02-17 23:54:36 -0300

    Adicionado CORS para permitir acesso do front-end

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
-import express, { Request, Response, NextFunction} from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 
@@ -12,11 +13,12 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction)  => {
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       status: 'error',
